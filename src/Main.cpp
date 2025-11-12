f9944d90d09705c47f6cb43136a76b06dfe071c4
Hexogen <nazhim.alakkash@gmail.com>
2025-11-12 09:42:09 +0200

    Fix include paths for src files

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,8 +12,8 @@
 #include <vector>   // Required for vector
 #include <memory>   // Required for unique_ptr
 
-#include "BoardGame_Classes.h"
-#include "XO_Classes.h"
+#include "../include/BoardGame_Classes.h"
+#include "../include/XO_Classes.h"
 using namespace std;
 
 /**
