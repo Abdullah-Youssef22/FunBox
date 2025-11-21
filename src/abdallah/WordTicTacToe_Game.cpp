c09a499e98f2c1934c321e91c52fc4ef9ffbb186
Hexogen <nazhim.alakkash@gmail.com>
2025-11-21 18:36:24 +0200

    added placeholders for the game files

---
+++ src/abdallah/WordTicTacToe_Game.cpp
@@ -0,0 +1,51 @@
+#include <iostream> // Required for input/output operations (cout, cin)
+#include <string>   // Required for string
+#include <vector>   // Required for vector
+#include <memory>   // Required for unique_ptr
+#include "../../include/util/BoardGame_Classes.h"
+#include "../../include/util/GameLaunchers.h"
+#include "../../include/SUS_Classes.h"
+using namespace std;
+
+/**
+ * @brief Main function to run the X-O game.
+ *
+ * This function orchestrates the game by:
+ * - Initializing the random number generator
+ * - Creating the X-O specific UI and board
+ * - Setting up players using the UI
+ * - Running the main game loop through the GameManager
+ * - Performing cleanup of dynamically allocated memory
+ *
+ * @return int Returns 0 on successful execution.
+ */
+void GameLaunchers::launch_word_tic_tac_toe() {
+    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+
+    // Create an instance of the specific UI for X-O using a pointer 
+    UI<char>* game_ui = new SUS_UI();
+
+    // Create the game board. For X-O, this is an X_O_Board.
+    Board<char>* xo_board = new SUS_Board();
+
+    // Use the UI to set up the players for the game.
+    // The UI returns a dynamically allocated array of Player pointers.
+    Player<char>** players = game_ui->setup_players();
+
+    // Create the game manager with the board and the array of players.
+    GameManager<char> x_o_game(xo_board, players, game_ui);
+
+    // Run the game loop.
+    x_o_game.run();
+
+    // --- Cleanup ---
+    // Delete the dynamically allocated board object.
+    delete xo_board;
+
+    // Delete the individual player objects.
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+    // Delete the dynamically allocated array of player pointers itself.
+    delete[] players;
+}
