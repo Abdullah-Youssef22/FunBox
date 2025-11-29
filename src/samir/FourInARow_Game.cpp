26538cb056cf0f88d736522ea5d89dd306fcf0d5
Hexogen <nazhim.alakkash@gmail.com>
2025-11-29 19:11:59 +0200

    fourinarow

--- src/samir/FourInARow_Game.cpp
+++ src/samir/FourInARow_Game.cpp
@@ -50,4 +50,7 @@ void GameLaunchers::launch_four_in_a_row() {
 
     // Delete the dynamically allocated array of player pointers itself.
     delete[] players;
+    cout << "\nGame Over! Press Enter to continue...";
+    cin.ignore();
+    cin.get();
 }
