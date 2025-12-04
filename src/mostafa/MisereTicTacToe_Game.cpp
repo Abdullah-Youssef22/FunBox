fb0a0d91db12404bf454a8521147bec6bba7e4db
Abdullah-Youssef22 <abdallah.jou3@gmail.com>
2025-12-04 17:54:20 +0200

    misary desplay();

--- src/mostafa/MisereTicTacToe_Game.cpp
+++ src/mostafa/MisereTicTacToe_Game.cpp
@@ -42,8 +42,5 @@ void GameLaunchers::launch_misere_tic_tac_toe() {
     // Delete the dynamically allocated array of player pointers itself.
     delete[] players;
 	delete game_ui;
-    cout << "\nGame Over! Press Enter to continue...";
-    cin.ignore();
-    cin.get();
 
 }
